0369cfa707cecde6a8c1b7ae80949b8ff4cf6385
Timothy Parker <tlparker71@gmail.com>
2022-05-25 20:26:56 -0700

    added null response

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 let favFruit = prompt("What is you favorite fruit?");
 console.log(favFruit);
-document.write(favFruit + "?");
+
 
 if (favFruit == "coconut"){
-    document.write(" Eww, that's really gross. Exit this page immediately");
+    document.write(favFruit + "? Eww, that's really gross. Exit this page immediately");
 } else if (favFruit == "strawberries"){
-    document.write(" I love those too!");
+    document.write(favFruit + "? I love those too!");
 } else if (favFruit == "plums"){
-    document.write(" I love those too!");
+    document.write(favFruit + "? I love those too!");
 } else if (favFruit == "blackberries"){
-    document.write(" I love those too!");
+    document.write(favFruit + "? I love those too!");
+} else if (favFruit == null){
+    document.write("Don't have one? You'll get scurvy!");
 } else {
-    document.write(" Solid choice!");
+    document.write(favFruit + "? Solid choice!");
 }
 
 function fruitRec() {
