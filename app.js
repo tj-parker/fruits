f218516ac780a4222e115f31e09119c5e697f268
Timothy Parker <tlparker71@gmail.com>
2022-05-26 21:49:01 -0700

    added more functions

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 let favFruit = prompt("What is you favorite fruit?");
 console.log(favFruit);
 
-
 function capitalize() {
     favFruitCap = favFruit.charAt(0).toUpperCase();
     favFruit = favFruitCap + favFruit.slice(1);
 }
 
-
 if (favFruit == null) {
     favFruit = prompt("Don't have one? You'll get scurvy! You gotta like something?");
 }
 
-if (favFruit == "coconut") {
-    capitalize();
-    document.write(favFruit + "? Eww, that's really gross. Exit this page immediately");
-} else if (favFruit == "strawberries" || "plums" || "blackberries") {
-    capitalize();
-    document.write(favFruit + "? I love those too!");
-} else {
-    capitalize();
-    document.write(favFruit + "? Solid choice!");
+function askFruit() {
+    if (favFruit == "coconut") {
+        capitalize();
+        document.write(favFruit + "? Eww, that's really gross. Exit this page immediately");
+    } else if (favFruit == "strawberries" || favFruit == "plums" || favFruit == "blackberries") {
+        capitalize();
+        document.write(favFruit + "? I love those too!");
+    } else if (favFruit == "apples" || favFruit == "oranges" || favFruit == "bananas" || favFruit == "mangos" || favFruit == "cherries") {
+        capitalize();
+        document.write(favFruit + "? Solid choice!");
+    } else {
+        capitalize();
+        favFruit = prompt(favFruit + "? That one's not on the list, might've forgot to add that to the database. Try another!");
+        askFruit();
+    }
 }
+
+askFruit();
 
 function fruitRec() {
     confirm("Anything but coconut!");
